05434c5b3a7d719848db97ff3a93b65dece9cd13
Vyacheslavchik <clavik_2000@mail.ru>
2023-11-17 00:45:14 +0300

    Добавьте файлы проекта.

---
+++ Avtorisacia.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Primer.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Primer
+{
+    
+    public partial class Avtorisacia : Window
+    {
+        private List <User> users;
+        public Avtorisacia()
+        {
+            InitializeComponent();
+            users = new List<User>();
+            users.Add(new User("админ", "admin", "qwerty"));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (emailBox.Text!=null&& passwordBox.Password!= null)
+            {
+                foreach (User user in users)
+                {
+                    if (user.email.Equals(emailBox.Text) && user.password.Equals(passwordBox.Password))
+                    {
+                        UserPage userPage = new UserPage();
+                        userPage.Show();
+                        this.Close();
+                    }
+                }
+                textExeptione.Text = "Неверный логин или пароль";
+                passwordBox.BorderBrush = new SolidColorBrush(Colors.Red);
+                emailBox.BorderBrush = new SolidColorBrush(Colors.Red);
+
+            }
+        }
+    }
+}
---
+++ Classes/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primer.Classes
+{
+    internal class User
+    {
+        public string firstName;
+        public string lastName;
+        public string password;
+        public string numberPhone;
+        public string email;
+
+
+        public User(string firstName, string email, string password)
+        {
+            this.firstName = firstName;
+            this.email = email;
+            this.password = password;
+        }
+
+       
+    }
+}
